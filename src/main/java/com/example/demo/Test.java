fa826e05b0d4cd5221e3f7985f520ceaeaca14ee
1633 <1049803789@qq.com>
2019-12-30 17:45:04 +0800

    first commit

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/test")
+public class Test {
+    @RequestMapping(value = "",method = RequestMethod.GET)
+    public void print(HttpServletResponse response){
+        try {
+            response.getWriter().println("hell");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
